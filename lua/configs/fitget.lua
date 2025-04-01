309d71840f8d4be804bca9a322099be76069ebf8
Hung (Patris) Nguyen <hung.np@cyberlogitec.com>
2025-04-01 20:53:48 +0700

    config lspsaga, fitget

---
+++ lua/configs/fitget.lua
@@ -0,0 +1,13 @@
+require("fidget").setup({
+  progress = {
+    display = {
+      done_icon = "✔️", -- Icon hoàn thành
+    },
+  },
+  notification = {
+    window = {
+      winblend = 0,       -- Không làm mờ nền
+      border = "rounded", -- Viền bo tròn
+    },
+  },
+})
