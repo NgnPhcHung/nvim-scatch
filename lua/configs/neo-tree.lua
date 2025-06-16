4b7e9abe4538d733574f97185ee9afce555f32f5
NGUYEN Phuc Hung <hung.np@cyberlogitec.com>
2025-06-16 21:46:21 +0700

    update config neotree

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -1,4 +1,3 @@
-
 require("neo-tree").setup({
   close_if_last_window = true,
   popup_border_style = "rounded",
@@ -10,6 +9,22 @@ require("neo-tree").setup({
     },
     bind_to_cwd = true,
     hijack_netrw_behavior = "open_current",
+
+    filtered_items = {
+      hide_dotfiles = true,
+      hide_git_ignored = true,
+      hide_hidden = true,
+      ignored_file_patterns = {
+        '^\\.env$', -- This pattern specifically matches and ignores ".env"
+        -- Add other patterns you want to ignore if necessary, e.g.:
+        -- 'node_modules',
+        -- '.git',
+        -- '%.o',
+        -- '%.obj',
+      },
+      -- If you want to show .env.*, you generally don't need a specific pattern
+      -- because the '^\\.env$' only matches exactly '.env' and not '.env.development'
+    },
   },
   buffers = {
     follow_current_file = {
@@ -29,4 +44,3 @@ require("neo-tree").setup({
     },
   }
 })
-
