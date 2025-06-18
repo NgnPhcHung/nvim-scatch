3d6106abd3b43f8dc256f7ff34d34ff6a2a2e311
NGUYEN Phuc Hung <hung.np@cyberlogitec.com>
2025-06-18 15:59:09 +0700

    chore: update alpha dashboard

--- lua/configs/alpha-nvim.lua
+++ lua/configs/alpha-nvim.lua
@@ -1,17 +1,23 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
-vim.defer_fn(function()
-  local total_plugins = #vim.tbl_keys(packer_plugins or {})
-  dashboard.section.footer.val = "Total plugins: " .. total_plugins
-  pcall(vim.cmd.AlphaRedraw)
-end, 100)
 dashboard.section.header.val =
     vim.fn.readfile(vim.fs.normalize("~/.config/nvim/lua/custom-config/files/my_header_banner.txt"))
 dashboard.section.header.opts.hl = "Question"
+
 dashboard.section.buttons.val = {
   dashboard.button("f", " Find file", ":Telescope find_files<CR>"),
-  dashboard.button("p", " Update plugins", ":Packer sync<CR>"),
+  dashboard.button("p", " Update plugins", ":Lazy sync<CR>"),
   dashboard.button("q", " Exit", ":qa!<CR>"),
 }
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  callback = function()
+    local stats = require("lazy").stats()
+    dashboard.section.footer.val = "Total plugins: " .. (stats.count or 0)
+    pcall(vim.cmd.AlphaRedraw)
+  end,
+})
+
 alpha.setup(dashboard.config)
